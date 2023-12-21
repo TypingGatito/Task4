aaf514e54ea1df15a3d3c753b94bf97484127eb1
karavaii <kirina205@gmail.com>
2023-12-21 14:50:53 +0300

    fix math tests

---
+++ tests/java/com/cgvsu/mathvector/VectorDimThreeTest.java
@@ -0,0 +1,80 @@
+package MathVector;
+
+import org.Math.Vector.VectorDimThree;
+import org.Math.Vector.VectorDimTwo;
+import org.testng.annotations.Test;
+
+import static org.testng.AssertJUnit.assertEquals;
+
+public class VectorDimThreeTest {
+    @Test
+    void sumVector(){
+        VectorDimThree v1 = new VectorDimThree(new float[]{-24, 23, 1.5f});
+        VectorDimThree v2 = new VectorDimThree(new float[]{24, -23, 2.3f});
+        VectorDimThree v3 = VectorDimThree.sumVector(v1, v2);
+        assertEquals(0.0f, v3.getV()[0]);
+        assertEquals(0.0f, v3.getV()[1]);
+        assertEquals(3.8f, v3.getV()[2]);
+    }
+
+    @Test
+    void subtractVector(){
+        VectorDimThree v1 = new VectorDimThree(new float[]{-24, 23, 1});
+        VectorDimThree v2 = new VectorDimThree(new float[]{24, -23, 2.3f});
+        VectorDimThree v3 = VectorDimThree.subtractVector(v1, v2);
+        assertEquals(-48.0f, v3.getV()[0]);
+        assertEquals(46.0f, v3.getV()[1]);
+        assertEquals(-1.3f, v3.getV()[2]);
+    }
+
+    @Test
+    void multiplyByScalar(){
+        VectorDimThree v1 = new VectorDimThree(new float[]{-24, 23, 1});
+        VectorDimThree v3 = VectorDimThree.multiplyByScalar((float) -10.0, v1);
+        assertEquals(240.0f, v3.getV()[0]);
+        assertEquals(-230.0f, v3.getV()[1]);
+        assertEquals(-10.0f, v3.getV()[2]);
+    }
+
+    @Test
+    void divideByScalar(){
+        VectorDimThree v1 = new VectorDimThree(new float[]{-25, 15, 45});
+        VectorDimThree v3 = VectorDimThree.divideByScalar((float) -10.0, v1);
+        assertEquals((float) 2.5, v3.getV()[0]);
+        assertEquals((float) -1.5, v3.getV()[1]);
+        assertEquals((float) -4.5, v3.getV()[2]);
+    }
+
+    @Test
+    void getVectorLength(){
+        VectorDimThree v1 = new VectorDimThree(new float[]{-6, 8, 0});
+        assertEquals((float) 10.0, VectorDimThree.getVectorLength(v1));
+    }
+
+    @Test
+    void normalize(){
+        VectorDimThree v1 = new VectorDimThree(new float[]{-6, 8, 0});
+        VectorDimThree v3 = VectorDimThree.normalize(v1);
+        assertEquals((float) -0.6, v3.getV()[0]);
+        assertEquals((float) 0.8, v3.getV()[1]);
+        assertEquals((float) 0.0, v3.getV()[2]);
+    }
+
+    @Test
+    void scaleMultiply(){
+        VectorDimThree v1 = new VectorDimThree(new float[]{-12, 3.5f, 2.3f});
+        VectorDimThree v2 = new VectorDimThree(new float[]{0.5F, 10, 10});
+        float res = VectorDimThree.scaleMultiply(v1, v2);
+        assertEquals(52.0f, res);
+    }
+
+    @Test
+    void vectorMultiplyV3(){
+        VectorDimThree v1 = new VectorDimThree(new float[]{1, 2, 3});
+        VectorDimThree v2 = new VectorDimThree(new float[]{2, 1, -2});
+        VectorDimThree v3 = VectorDimThree.vectorMultiplyV3(v1, v2);
+        assertEquals((float) -7.0, v3.getV()[0]);
+        assertEquals((float) 8.0, v3.getV()[1]);
+        assertEquals((float) -3.0, v3.getV()[2]);
+    }
+}
---
+++ tests/java/com/cgvsu/matrix/MatrixDimThreeTest.java
@@ -0,0 +1,106 @@
+package Matrix;
+
+import org.Math.Matrix.MatrixDimThree;
+import org.Math.Vector.VectorDimThree;
+import org.testng.annotations.Test;
+
+import static org.testng.AssertJUnit.assertEquals;
+
+public class MatrixDimThreeTest {
+    @Test
+    void sumMatrix(){
+        MatrixDimThree m1 = new MatrixDimThree(new float[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}});
+        MatrixDimThree m2 = new MatrixDimThree(new float[][]{{0, 1, 2}, {3, 4, 5}, {6, 7, 8}});
+        MatrixDimThree m3 = new MatrixDimThree(MatrixDimThree.sumMatrix(m1, m2).getM1());
+        MatrixDimThree m4 = new MatrixDimThree(new float[][]{{1, 3, 5}, {7, 9, 11}, {13, 15, 17}});
+        for (int i = 0; i < m1.getM1().length; i++){
+            for (int j = 0; j < m3.getM1()[0].length; j++) {
+                assertEquals(m4.getM1()[i][j], m3.getM1()[i][j]);
+            }
+        }
+    }
+
+    @Test
+    void subtractMatrix(){
+        MatrixDimThree m1 = new MatrixDimThree(new float[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}});
+        MatrixDimThree m2 = new MatrixDimThree(new float[][]{{0, 1, 2}, {3, 4, 5}, {6, 7, 8}});
+        MatrixDimThree m3 = new MatrixDimThree(MatrixDimThree.subtractMatrix(m1, m2).getM1());
+        for (int i = 0; i < m1.getM1().length; i++){
+            for (int j = 0; j < m3.getM1()[0].length; j++) {
+                assertEquals(1f, m3.getM1()[i][j]);
+            }
+        }
+    }
+
+    @Test
+    void mMultV(){
+        MatrixDimThree m1 = new MatrixDimThree(new float[][]{{1, 2, 3}, {1, 2, 3}, {1, 2, 3}});
+        VectorDimThree v1 = new VectorDimThree(new float[]{1, 1, 0});
+        VectorDimThree v = new VectorDimThree(MatrixDimThree.mMultV(m1, v1).getV());
+        for (int i = 0; i < v.getV().length; i++){
+            assertEquals( 3f, v.getV()[i]);
+        }
+    }
+
+    @Test
+    void multMatrix(){
+        MatrixDimThree m1 = new MatrixDimThree(new float[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}});
+        MatrixDimThree m2 = new MatrixDimThree(new float[][]{{0, 1, 2}, {3, 4, 5}, {6, 7, 8}});
+        MatrixDimThree m3 = new MatrixDimThree(MatrixDimThree.multMatrix(m1, m2).getM1());
+        MatrixDimThree m4 = new MatrixDimThree(new float[][]{{24, 30, 36}, {51, 66, 81}, {78, 102, 126}});
+        for (int i = 0; i < m4.getM1().length; i++){
+            for (int j = 0; j < m4.getM1()[0].length; j++) {
+                assertEquals(m4.getM1()[i][j], m3.getM1()[i][j]);
+            }
+        }
+    }
+
+    @Test
+    void transposition(){
+        MatrixDimThree m = new MatrixDimThree(new float[][]{{0, 1, 2}, {3, 4, 5}, {6, 7, 8}});
+        MatrixDimThree m1 = new MatrixDimThree(MatrixDimThree.transposition(m).getM1());
+        MatrixDimThree m2 = new MatrixDimThree(new float[][]{{0, 3, 6}, {1, 4, 7}, {2, 5, 8}});
+        for (int i = 0; i < m2.getM1().length; i++){
+            for (int j = 0; j < m2.getM1()[0].length; j++) {
+                assertEquals(m2.getM1()[i][j], m1.getM1()[i][j]);
+            }
+        }
+    }
+
+    @Test
+    void zeroMatrix(){
+        MatrixDimThree m0 = new MatrixDimThree(MatrixDimThree.zeroMatrix().getM1());
+        MatrixDimThree m = new MatrixDimThree(new float[3][3]);
+        for (int i = 0; i < m.getM1().length; i++){
+            for (int j = 0; j < m.getM1()[0].length; j++) {
+                assertEquals(0.0f, m.getM1()[i][j]);
+            }
+        }
+    }
+
+    @Test
+    void eyeMatrix(){
+        MatrixDimThree m1 = new MatrixDimThree(MatrixDimThree.eyeMatrix1().getM1());
+        MatrixDimThree m = new MatrixDimThree(new float[][]{{1, 0, 0}, {0, 1, 0}, {0, 0, 1}});
+        for (int i = 0; i < m.getM1().length; i++){
+            for (int j = 0; j < m.getM1()[0].length; j++) {
+                assertEquals(m.getM1()[i][j], m1.getM1()[i][j]);
+            }
+        }
+    }
+
+    @Test
+    void getDeterminant(){
+        MatrixDimThree m = new MatrixDimThree(new float[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}});
+        assertEquals(0.0f, MatrixDimThree.getDeterminant(m));
+    }
+
+    /*@Test
+    void reverseMatrix(){
+        MatrixDimThree m = new MatrixDimThree(new float[]{2, 5, 7, 6, 3, 4, 5, -2, -3});
+        MatrixDimThree mRev = new MatrixDimThree(new float[]{1, -1, 1, -38, 41, -34, 27, -29, 24});
+        for (int i = 0; i < m.getM().length; i++){
+            assertEquals(m.getM()[i], mRev.getM()[i]);
+        }
+    }*/
+}
