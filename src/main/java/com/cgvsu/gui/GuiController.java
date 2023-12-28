14fcb74a917c2a4dcd78a42da00e52e826c16da7
Igor <darthveider36@gmail.com>
2023-12-28 22:24:51 +0300

    Some changes in the interface.

--- src/main/java/com/cgvsu/gui/GuiController.java
+++ src/main/java/com/cgvsu/gui/GuiController.java
@@ -7,11 +7,13 @@ import com.cgvsu.draw.modes.CameraController;
 import com.cgvsu.infoclasses.ModelsInfo;
 import com.cgvsu.math.vector.VectorDimThree;
 import com.cgvsu.objreader.ObjReader;
+import com.cgvsu.objwriter.ObjWriter;
 import com.cgvsu.render_engine.Camera;
 import com.cgvsu.render_engine.RenderEngine;
 import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
+import javafx.application.Platform;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -21,7 +23,6 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.FileChooser;
-import javafx.stage.Stage;
 import javafx.util.Duration;
 
 import java.io.File;
@@ -29,7 +30,6 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class GuiController {
     final private float TRANSLATION = 0.5F;
@@ -52,10 +52,10 @@ public class GuiController {
 
     //позже удалить отдельную камеру
     private CameraController cameraController = new CameraController();
-/*    private Camera camera = new Camera(
-            new VectorDimThree(0, 0, 100),
-            new VectorDimThree(0, 0, 0),
-            1.0F, 1, 0.01F, 100);*/
+    /*    private Camera camera = new Camera(
+                new VectorDimThree(0, 0, 100),
+                new VectorDimThree(0, 0, 0),
+                1.0F, 1, 0.01F, 100);*/
 
     public GuiController() {
         Camera camera = new Camera(
@@ -79,6 +79,8 @@ public class GuiController {
 
         columnModelActive.setSortable(false);
         columnModelName.setSortable(false);
+        columnModelName.setReorderable(false);
+        columnModelActive.setReorderable(false);
 
         columnModelName.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getModelName()));
         columnModelActive.setCellFactory(col -> new TableCell<>() {
@@ -144,13 +146,13 @@ public class GuiController {
     }
 
     @FXML
-    private void addModelButtonClick() {
+    private void addOnButtonClick() {
         FileChooser fileChooser = new FileChooser();
         fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Model (*.obj)", "*.obj"));
         fileChooser.setInitialDirectory(new File("./3DModels")); // для удобства при тестировании
         fileChooser.setTitle("Load Model");
 
-        File file = fileChooser.showOpenDialog((Stage) canvas.getScene().getWindow());
+        File file = fileChooser.showOpenDialog(canvas.getScene().getWindow());
         if (file == null) {
             return;
         }
@@ -162,14 +164,13 @@ public class GuiController {
             Model model = ObjReader.read(fileContent);
 
             String fileNameString = fileName.toString();
-            String shortName = fileNameString.substring(fileNameString.lastIndexOf("\\") + 1);
+            String shortName = fileNameString.substring(fileNameString.lastIndexOf("\\") + 1, fileNameString.length() - 4);
 
             tableModels.getItems().add(new ModelData(model, shortName, true));
 
             sceneModels.addModel(model);
             sceneModels.chooseModel(model);
-            modelsInfo.addModel(model);
-            modelsInfo.addModelFilename(model, fileContent);
+            modelsInfo.addModelFilename(model, fileNameString);
             modelsInfo.addModelTriangulated(model, new ModelTriangulated(model));
             // todo: обработка ошибок
         } catch (IOException exception) {
@@ -178,7 +179,7 @@ public class GuiController {
     }
 
     @FXML
-    private void removeModelButtonClick(ActionEvent actionEvent) {
+    private void deleteSelectedOnButtonClick() {
         ObservableList<ModelData> selectedModelsData = tableModels.getSelectionModel().getSelectedItems();
         List<Model> selectedModels = selectedModelsData.stream()
                 .map(ModelData::getModel)
@@ -186,6 +187,66 @@ public class GuiController {
         selectedModels.forEach((m) -> sceneModels.getModelMatrixInfoMap().remove(m));
         selectedModels.forEach((m) -> sceneModels.getChosenModels().remove(m));
         tableModels.getItems().removeAll(selectedModelsData);
+    }
+
+    @FXML
+    private void saveSelectedOnButtonClick() {
+        ObservableList<ModelData> selectedModelsData = tableModels.getSelectionModel().getSelectedItems();
+        List<Model> selectedModels = selectedModelsData.stream()
+                .map(ModelData::getModel)
+                .toList();
+        selectedModels.forEach((m) -> ObjWriter.write(modelsInfo.getMadelFilenameMap().get(m), m));
+        tableModels.getItems().removeAll(selectedModelsData);
+    }
+
+    @FXML
+    private void saveSelectedAsOnButtonClick() {
+        ObservableList<ModelData> selectedModelsData = tableModels.getSelectionModel().getSelectedItems();
+        List<Model> selectedModels = selectedModelsData.stream()
+                .map(ModelData::getModel)
+                .toList();
+
+        FileChooser fileChooser = new FileChooser();
+
+        for (Model model : selectedModels) {
+            String modelName = modelsInfo.getMadelFilenameMap().get(model);
+            String shortName = modelName.substring(modelName.lastIndexOf("\\") + 1, modelName.length() - 4);
+
+            fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Model (*.obj)", "*.obj"));
+            fileChooser.setInitialDirectory(new File("./")); // для удобства при тестировании
+
+            fileChooser.setTitle(String.format("Save %s as", shortName));
+
+            File file = fileChooser.showSaveDialog(canvas.getScene().getWindow());
+
+            if (file != null) {
+                Path fileName = Path.of(file.getAbsolutePath());
+                ObjWriter.write(fileName.toString(), model);
+            }
+        }
+    }
+
+    @FXML
+    private void exitOnButtonClick() {
+        //todo: if (есть несохранённые изменения) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Confirm");
+        alert.setHeaderText("Unsaved changes have been made. Do you want to save them?");
+
+        ButtonType yesButton = new ButtonType("Yes");
+        ButtonType noButton = new ButtonType("No");
+        ButtonType cancelButton = new ButtonType("Cancel");
+
+        alert.getButtonTypes().setAll(yesButton, noButton, cancelButton);
+
+        alert.showAndWait().ifPresent(buttonType -> {
+            if (buttonType == noButton) {
+                Platform.exit();
+            } else if (buttonType == yesButton) {
+                //todo: save all unsaved changes
+                Platform.exit();
+            }
+        });
     }
 
     @FXML
