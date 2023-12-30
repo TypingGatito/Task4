f7cde908934262387c3fc8cec57d1940e0bb00cc
Igor <darthveider36@gmail.com>
2023-12-30 22:38:59 +0300

    Change images names and add dock icon for macOS.

--- src/main/java/com/cgvsu/Simple3DViewer.java
+++ src/main/java/com/cgvsu/Simple3DViewer.java
@@ -1,6 +1,5 @@
 package com.cgvsu;
 
-import com.cgvsu.components.SceneModels;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -8,6 +7,7 @@ import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
+import java.awt.*;
 import java.io.IOException;
 import java.util.Objects;
 
@@ -18,8 +18,8 @@ public class Simple3DViewer extends Application {
         AnchorPane viewport = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("fxml/gui.fxml")));
 
         Scene scene = new Scene(viewport);
-        stage.setMinWidth(1600);
-        stage.setMinHeight(900);
+        stage.setMinWidth(1000);
+        stage.setMinHeight(600);
         viewport.prefWidthProperty().bind(scene.widthProperty());
         viewport.prefHeightProperty().bind(scene.heightProperty());
 
@@ -27,7 +27,18 @@ public class Simple3DViewer extends Application {
         scene.getStylesheets().add(css);
 
         stage.setTitle("RenderCat Pro");
-        stage.getIcons().add(new Image(Objects.requireNonNull(getClass().getResource("images/RenderCat Pro Icon.png")).toExternalForm()));
+        stage.getIcons().add(new Image(Objects.requireNonNull(getClass().getResource("images/icon.png")).toExternalForm()));
+
+        if (Taskbar.isTaskbarSupported()) { // код для того, чтобы на macOS была иконка приложения...
+            var taskbar = Taskbar.getTaskbar();
+
+            if (taskbar.isSupported(Taskbar.Feature.ICON_IMAGE)) {
+                final Toolkit defaultToolkit = Toolkit.getDefaultToolkit();
+                var dockIcon = defaultToolkit.getImage(getClass().getResource("images/icon.png"));
+                taskbar.setIconImage(dockIcon);
+            }
+
+        }
         stage.setScene(scene);
         stage.show();
     }
